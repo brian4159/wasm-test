44affba963f720ba217d2f9fd84dee2ad3eb84f8
brian <brian4159@163.com>
2024-07-24 21:37:19 +0800

    rust编译为wasm文件

--- www/index.js
+++ www/index.js
@@ -1,24 +1,5 @@
-async function run(){
+import initSync,{greet} from "wasm_game";
 
-    const importObj = {
-        console:{
-            log:()=>{
-                console.log('hello world');
-            },
-            error:()=>{
-                console.log('error');
-            }
-        }
-    }
-    const response = await fetch('test2.wasm')
-    const buffer = await response.arrayBuffer()
-    const wasm = await WebAssembly.instantiate(buffer,importObj)
-
-    const addTwoFunc = wasm.instance.exports.addTwo
-     
-
-    const result  = addTwoFunc(2,2)
-    console.log(result);
-}
-
-run()
+initSync().then(()=>{
+    greet("world");
+})
